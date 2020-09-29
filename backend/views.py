f135f7ad3fb05f799a436266d834a73cbcff11c7
Edwin Zamudio <ezamudio@hawk.iit.edu>
2020-09-29 14:59:54 -0500

    created django repo and added bootstrap

---
+++ backend/views.py
@@ -0,0 +1,11 @@
+import os
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def home(request):
+    return render(request, 'base.html')
