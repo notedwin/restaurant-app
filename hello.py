0b06803097c15a58cb88c0d4ebdfe8edbee829f0
Edwin Zamudio <ezamudio@hawk.iit.edu>
2020-11-05 08:27:42 -0600

    add staff portal

--- hello.py
+++ hello.py
@@ -2,7 +2,9 @@ from flask import Flask, request, session, redirect, url_for, render_template, f
 from flask_bootstrap import Bootstrap
 from flask_fontawesome import FontAwesome
 
+
 app = Flask(__name__,static_url_path='/static',static_folder='static')
+#app.config.from_pyfile('config.cfg')
 Bootstrap(app)
 FontAwesome(app)
 
@@ -10,18 +12,41 @@ FontAwesome(app)
 def index():
     return render_template('index.html')
 
+@app.route('/order',methods=['GET','POST'])
+def order():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'Do Something':
+            pass # do something
+        elif request.form['submit_button'] == 'Do Something Else':
+            pass # do something else
+        else:
+            pass # unknown
+    elif request.method == 'GET':
+        return render_template('customer/order.html')
+
+
 @app.route('/menu')
 def menu():
-    return render_template('menu.html')
+    return render_template('customer/menu.html')
 
 @app.route('/cart')
 def cart():
-    return render_template('cart.html')
+    return render_template('customer/cart.html')
 
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'GET' :
-        return render_template('login.html')
+        return render_template('customer/login.html')
+
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'GET' :
+        return render_template('customer/register.html')
+
+@app.route('/staff-portal')
+def staff():
+    #ask to sign in
+    return render_template('staff/staff-portal.html')
 
 if __name__ == '__main__':
     app.jinja_env.auto_reload = True
